a348b663f576d91cad755d3e09374ee874464c99
sb6998 <39883172+sb6998@users.noreply.github.com>
2020-09-26 15:31:16 +0530

    Add files via upload

--- xgboost_.py
+++ xgboost_.py
@@ -1,15 +1,16 @@
+  
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.naive_bayes import MultinomialNB
+from xgboost import XGBClassifier
 from sklearn.metrics import classification_report
 
-def naive_bayes(feat, tar, split):
+def xgboost_(feat, tar, split):
     ms = MinMaxScaler()
     X_train, X_test, y_train, y_test = train_test_split(feat,tar,test_size=split,random_state=101)
     ms.fit(X_train)
     X_train = ms.transform(X_train)
     X_test = ms.transform(X_test)
-    nb = MultinomialNB()
-    nb.fit(X_train,y_train)
-    predictions = nb.predict(X_test)
+    classifier = XGBClassifier()
+    classifier.fit(X_train, y_train)
+    predictions = classifier.predict(X_test)
     print(classification_report(y_test,predictions))
